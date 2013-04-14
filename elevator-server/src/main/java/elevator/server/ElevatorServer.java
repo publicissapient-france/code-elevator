43d7fb33ff36a49ed0da682914c784e502c0c110
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-04-14 15:30:15 +0200

    jersey's PackagesResourceConfig seems to be broken with java8

--- elevator-server/src/main/java/elevator/server/ElevatorServer.java
+++ elevator-server/src/main/java/elevator/server/ElevatorServer.java
@@ -1,7 +1,7 @@
 package elevator.server;
 
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
-import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ClassNamesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.net.httpserver.HttpServer;
 
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ElevatorServer {
 
     public StartedElevatorServer start() {
-        ResourceConfig resourceConfig = new PackagesResourceConfig("elevator.server");
+        ResourceConfig resourceConfig = new ClassNamesResourceConfig(ElevatorResource.class);
         try {
             HttpServer httpServer = HttpServerFactory.create("http://localhost:" + new RandomPort().port + "/", resourceConfig);
             httpServer.start();
