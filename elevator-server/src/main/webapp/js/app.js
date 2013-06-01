22471694c77dc7d949415e597ad9c06cb80e4db6
yannick grenzinger <ygrenzinger@xebia.fr>
2013-06-01 09:50:01 +0200

    design

--- elevator-server/src/main/webapp/js/app.js
+++ elevator-server/src/main/webapp/js/app.js
@@ -1,9 +1,9 @@
 angular.module('elevatorApp', ['ngCookies']).
     config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.
-            when('/elevator.html', {templateUrl: 'partials/elevator.html', controller: ElevatorCtrl}).
-            when('/leaderboard.html', {templateUrl: 'partials/leaderboard.html', controller: LeaderboardCtrl}).
-            otherwise({redirectTo: '/elevator.html'});
+            when('/elevator', {templateUrl: 'partials/elevator.html', controller: ElevatorCtrl}).
+            when('/leaderboard', {templateUrl: 'partials/leaderboard.html', controller: LeaderboardCtrl}).
+            otherwise({redirectTo: '/elevator'});
         $locationProvider.
             html5Mode(true);
     }]);
