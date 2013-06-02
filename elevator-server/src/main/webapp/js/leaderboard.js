03579b8ce968b1fd76b270a6e08eec458d7fe3b2
yannick grenzinger <ygrenzinger@xebia.fr>
2013-06-02 13:45:34 +0200

    Leaderboard working!

--- elevator-server/src/main/webapp/js/leaderboard.js
+++ elevator-server/src/main/webapp/js/leaderboard.js
@@ -1,55 +1,21 @@
 'use strict';
 
-function LeaderboardCtrl($scope, $timeout) {
-        $scope.players = [];
-        $scope.numberOfFloors = 5;
+function LeaderboardCtrl($scope, $http, $timeout) {
+    $scope.players = [];
 
-        var numberOfPlayers = 20;
+    function fetchLeaderboard($scope, $http, $timeout) {
+        (function fetch() {
+            $http.get('/resources/leaderboard')
+                .success(function (data) {
+                    $scope.players = data;
+                });
+            $timeout(fetch, 1000);
+        })();
+    }
 
-        for (var i = 0; i < numberOfPlayers; i++) {
-            var player = {
-                name: 'Player ' + i,
-                score: 0,
-                elevatorAtFloor: $scope.numberOfFloors - 1,
-                peopleInTheElevator: Math.floor(Math.random() * 4)
-            }
+    fetchLeaderboard($scope, $http, $timeout);
 
-            var peopleWaitingTheElevator = [];
-            for (var j = 0; j < $scope.numberOfFloors; j++) {
-                peopleWaitingTheElevator.push(Math.floor(Math.random() * 4));
-            }
-            player.peopleWaitingTheElevator = peopleWaitingTheElevator;
-
-            $scope.players.push(player);
-        }
-
-
-        randomizeScore($scope, $timeout);
-
-        setInterval(function(){
-        },3000);
-
-
-    };
-
-LeaderboardCtrl.$inject = ['$scope', '$timeout'];
-
-
-function randomizeScore($scope, $timeout) {
-    (function randomize() {
-        for (var i = 0; i < $scope.players.length; i++) {
-            var player = $scope.players[i];
-            player.score = Math.floor(Math.random() * 10);
-            player.elevatorAtFloor = Math.floor(Math.random() * 4);
-
-            var peopleWaitingTheElevator = [];
-            for (var j = 0; j < $scope.numberOfFloors; j++) {
-                peopleWaitingTheElevator.push(Math.floor(Math.random() * 4));
-            }
-            player.peopleWaitingTheElevator = peopleWaitingTheElevator;
-
-        }
-
-        $timeout(randomize, 1000);
-    })();
 }
+
+
+LeaderboardCtrl.$inject = ['$scope', '$http', '$timeout'];
