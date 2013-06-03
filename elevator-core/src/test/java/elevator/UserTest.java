cb3faef725f3ef90d843f935c48fd2ee453231d5
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-06-04 01:02:11 +0200

    avoid to instantiate NaiveElevator

--- elevator-core/src/test/java/elevator/UserTest.java
+++ elevator-core/src/test/java/elevator/UserTest.java
@@ -1,15 +1,22 @@
 package elevator;
 
-import elevator.engine.naive.NaiveElevator;
+import elevator.engine.ElevatorEngine;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserTest {
+
+    @Mock
+    ElevatorEngine mockElevatorEngine;
 
     @Test
     public void should_not_count_tick_to_go_when_not_traveling() {
-        User user = new User(new NaiveElevator());
+        User user = new User(mockElevatorEngine);
 
         user.onTick();
 
@@ -18,7 +25,7 @@ public class UserTest {
 
     @Test
     public void should_count_tick_to_go_when_traveling() {
-        User user = new User(new NaiveElevator());
+        User user = new User(mockElevatorEngine);
 
         //Make the user entering the elevator=> traveling
         user.elevatorIsOpen(user.getFloor());
@@ -30,7 +37,7 @@ public class UserTest {
 
     @Test
     public void should_not_count_tick_to_wait_when_not_waiting() {
-        User user = new User(new NaiveElevator());
+        User user = new User(mockElevatorEngine);
 
         //Make the user entering the elevator=> traveling
         user.elevatorIsOpen(user.getFloor());
@@ -42,10 +49,11 @@ public class UserTest {
 
     @Test
     public void should_count_tick_to_wait_when_waiting() {
-        User user = new User(new NaiveElevator());
+        User user = new User(mockElevatorEngine);
 
         user.onTick();
 
         assertThat(user.getTickToWait()).isEqualTo(1);
     }
+
 }
