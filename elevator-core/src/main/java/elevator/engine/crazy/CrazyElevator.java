72d43d5734396e6e38ee42adbfef82c6d3f84cd3
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-06-26 02:09:23 +0200

    add a crazy elevator engine to do monkey tests

---
+++ elevator-core/src/main/java/elevator/engine/crazy/CrazyElevator.java
@@ -0,0 +1,85 @@
+package elevator.engine.crazy;
+
+import elevator.Command;
+import elevator.Direction;
+import elevator.User;
+import elevator.engine.ElevatorEngine;
+import elevator.engine.naive.NaiveElevator;
+
+import static java.lang.Math.random;
+
+public class CrazyElevator implements ElevatorEngine {
+
+    private ElevatorEngine underlyingElevator;
+
+    public CrazyElevator() {
+        underlyingElevator = new NaiveElevator();
+    }
+
+    @Override
+    public ElevatorEngine call(Integer atFloor, Direction to) {
+        crazyWait();
+        return this;
+    }
+
+    @Override
+    public ElevatorEngine go(Integer floorToGo) {
+        crazyWait();
+        return this;
+    }
+
+    @Override
+    public Command nextCommand() {
+        crazyWait();
+        return crazyCommand();
+    }
+
+    @Override
+    public ElevatorEngine userHasEntered(User user) {
+        crazyWait();
+        return this;
+    }
+
+    @Override
+    public ElevatorEngine userHasExited(User user) {
+        crazyWait();
+        return this;
+    }
+
+    @Override
+    public ElevatorEngine reset() {
+        underlyingElevator.reset();
+        return this;
+    }
+
+    private void crazyWait() {
+        if (crazy()) {
+            try {
+                Long secondsToWait = new Double(random() * 10).longValue();
+                System.out.println("waiting for " + secondsToWait + "s");
+                Thread.sleep(secondsToWait * 1000);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private Command crazyCommand() {
+        Command correctCommand = underlyingElevator.nextCommand();
+        if (crazy()) {
+            Command crazyCommand = Command.values()[new Double(random() * 5).intValue()];
+            System.out.println("send " + crazyCommand + " instead of " + correctCommand);
+            return crazyCommand;
+        }
+        return correctCommand;
+    }
+
+    private Boolean crazy() {
+        Boolean crazy = random() * 5 > 4d;
+        if (crazy) {
+            System.out.print("CRAZY ");
+        }
+        return crazy;
+    }
+
+}
