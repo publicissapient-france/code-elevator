a6fd4551b0f4b69e835a532a02a42abe02ffcf09
Eric Le Merdy <eric.lemerdy@laposte.net>
2013-08-04 18:39:53 +0200

    Fix todo by printing admin urls in console server.

--- elevator-server/src/main/java/elevator/server/security/RandomPassword.java
+++ elevator-server/src/main/java/elevator/server/security/RandomPassword.java
@@ -1,16 +1,18 @@
 package elevator.server.security;
 
 import static java.lang.Math.random;
+import static java.lang.String.format;
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 
 public class RandomPassword implements Password {
 
     public static final String password = generate();
 
     private static String generate() {
-        final String chars = "" +
-                "0123456789" +
-                "abcdefghijklmnopqrstuvwxyz" +
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        final String chars = "" + //
+                "0123456789" + //
+                "abcdefghijklmnopqrstuvwxyz" + //
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + //
                 "_-";
 
         final StringBuilder password = new StringBuilder(16);
@@ -18,6 +20,12 @@ public class RandomPassword implements Password {
         for (Integer i = 0; i < 16; i++) {
             password.append(chars.charAt(new Double(random() * chars.length()).intValue()));
         }
+
+        String authToken = printBase64Binary(("admin:" + password.toString()).getBytes());
+        String curl = "curl --header 'Authorization: Basic %s' 'http://localhost:8080/resources/admin/%s'";
+        System.out.println(format(curl, authToken, "maxNumberOfUsers"));
+        System.out.println(format(curl, authToken, "increaseMaxNumberOfUsers"));
+        System.out.println(format(curl, authToken, "decreaseMaxNumberOfUsers"));
 
         return password.toString();
     }
