50eafa304511c0f9f9ccef028e495116cda709fc
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-08-16 19:09:05 +0200

    verify stop behavior

--- elevator-server/src/test/java/elevator/server/ElevatorGameTest.java
+++ elevator-server/src/test/java/elevator/server/ElevatorGameTest.java
@@ -2,11 +2,15 @@ package elevator.server;
 
 import elevator.Clock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.net.URL;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ElevatorGameTest {
 
     @Test(expected = IllegalArgumentException.class)
@@ -22,6 +26,16 @@ public class ElevatorGameTest {
 
         assertThat(elevatorGame.score.score).isEqualTo(-10);
         assertThat(elevatorGame.lastErrorMessage).isEqualTo("error message");
+    }
+
+    @Test
+    public void should_stop() throws Exception {
+        Clock clock = spy(new Clock());
+        ElevatorGame elevatorGame = new ElevatorGame(new Player("player@provider.com", "player"), new URL("http://localhost"), null, clock);
+
+        elevatorGame.stop();
+
+        verify(clock, times(1)).removeClockListener(elevatorGame);
     }
 
 }
