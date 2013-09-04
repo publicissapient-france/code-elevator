5f4cfd24821bab1a2826e377df45906659a9fe87
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-09-04 21:13:31 +0200

    Adding script to run the server whithout having to re-read the readme each time

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ ! -f elevator-server/target/elevator-server-1.0-SNAPSHOT.war ];
+then
+	echo "\nNothin' ready, building binaries\n\n"
+	mvn clean install
+	if [ $? -ne 0 ]; then
+		echo "\nc'mon, can't build, can't run the server...\n\n"
+		exit $?
+	fi
+fi
+echo "\nRunnin' server\n\n"
+mvn --file elevator-server jetty:run
