ed6eeef89df31d2a678fcd28ff58e72d29df13f6
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-10-08 09:21:47 +0200

    default admin password could be overrided by system property

--- elevator-server/src/main/java/elevator/server/security/AdminPassword.java
+++ elevator-server/src/main/java/elevator/server/security/AdminPassword.java
@@ -2,9 +2,17 @@ package elevator.server.security;
 
 class AdminPassword implements Password {
 
+    static final String PASSWORD_ROPERTY = "ADMIN_PASSWORD";
+
+    private final String password;
+
+    AdminPassword() {
+        this.password = System.getProperty(PASSWORD_ROPERTY, "admin");
+    }
+
     @Override
     public String value() {
-        return "admin";
+        return password;
     }
 
 }
--- elevator-server/src/test/java/elevator/server/security/AdminPasswordTest.java
+++ elevator-server/src/test/java/elevator/server/security/AdminPasswordTest.java
@@ -1,16 +1,31 @@
 package elevator.server.security;
 
+import elevator.server.port.SystemPropertyRule;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static elevator.server.security.AdminPassword.PASSWORD_ROPERTY;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class AdminPasswordTest {
+
+    @Rule
+    public SystemPropertyRule systemPropertyRule = new SystemPropertyRule("ADMIN_PASSWORD");
 
     @Test
     public void should_have_default_password() {
         final Password password = new AdminPassword();
 
         assertThat(password.value()).isEqualTo("admin");
+    }
+
+    @Test
+    public void should_override_default_password_with_system_property() {
+        System.setProperty(PASSWORD_ROPERTY, "an admin password");
+
+        final Password password = new AdminPassword();
+
+        assertThat(password.value()).isEqualTo("an admin password");
     }
 
 }
