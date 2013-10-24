bfff8aba44e6468e0dc63a9175be1ad8143b71af
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-10-24 23:07:55 +0200

    handle admin password when deploying

--- elevator-server/bees.sh
+++ elevator-server/bees.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
 
-usage='Usage: ./bees.sh password url appid appid\n\tEg: ./bees.sh foobar http://foo.bar.eu.cloudbees.net bar/foo'
+usage='Usage: ./bees.sh url appid [password]\n\tEg: ./bees.sh http://foo.bar.eu.cloudbees.net bar/foo secret'
 
 if [ -z "$1" ]
-then
-    echo "Missing elevator server password"
-    echo $usage
-    exit 0
-fi
-
-if [ -z "$2" ]
 then
     echo "Missing elevator server url"
     echo $usage
     exit 0
 fi
 
-if [ -z "$3" ]
+if [ -z "$2" ]
 then
     echo "Missing CloudBees appid"
     echo $usage
     exit 0
 fi
 
-curl --basic --user :$1 --url $2/resources/players.csv > players.csv
+if [ -z "$3" ]
+then
+    echo "Using default password"
+    PASSWORD="admin"
+else
+    PASSWORD_BEES="-P ADMIN_PASSWORD=$3"
+    PASSWORD=$3
+fi
+
+curl --basic --user :$PASSWORD --url $1/resources/players.csv > players.csv
 echo "Players exported in players.csv"
 
-bees app:deploy --appid $3 --endPoint eu --type tomcat7 target/*.war
+bees app:deploy --appid $2 --endPoint eu $PASSWORD_BEES --type tomcat7 target/*.war
+echo bees app:deploy --appid $2 --endPoint eu $PASSWORD_BEES --type tomcat7 target/*.war
 
-curl --basic --user :$1 --form players=@players.csv --url $2/resources/players.csv > uploadResult.txt
+curl --basic --user :$PASSWORD --form players=@players.csv --url $1/resources/players.csv > uploadResult.txt
 echo "Players imported. Result in uploadResult.txt"
