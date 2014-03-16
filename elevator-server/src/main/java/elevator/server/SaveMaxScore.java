50923e206cc1288b1ece518e74be2c0667de795c
joe-mojo <joachim.e.rousseau+github@gmail.com>
2014-03-16 20:39:28 +0100

    Cleaning

--- elevator-server/src/main/java/elevator/server/SaveMaxScore.java
+++ elevator-server/src/main/java/elevator/server/SaveMaxScore.java
@@ -1,10 +1,9 @@
 package elevator.server;
 
-import java.io.*;
-import java.util.*;
+import java.util.Observable;
+import java.util.Observer;
 
 class SaveMaxScore implements Observer {
-//    static final File SCORES_FILE = new File("scores");
 
     private final Player player;
 
@@ -24,54 +23,4 @@ class SaveMaxScore implements Observer {
 			storageSvc.saveScoreIfBetter((Score) o, player);
 		}
     }
-
-    /*private synchronized void saveIfNotPresentOrGreater() {
-        try {
-            Map<String, Integer> playerAndScores = readScores();
-            if (!playerAndScores.containsKey(player.email) || playerAndScores.get(player.email) < score.score) {
-                writeScores(playerAndScores);
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    /*private static Map<String, Integer> readScores() throws IOException, ClassNotFoundException {
-        if (!SCORES_FILE.exists()) {
-            return new HashMap<>();
-        }
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(SCORES_FILE))) {
-            return (Map<String, Integer>) in.readObject();
-        }
-    }*/
-
-    /*private void writeScores(Map<String, Integer> playerAndScores) throws IOException {
-        playerAndScores.put(player.email, score.score);
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(SCORES_FILE))) {
-            out.writeObject(playerAndScores);
-        }
-    }*/
-
-    /*public static void main(String[] args) throws IOException, ClassNotFoundException {
-        for (Map.Entry<Integer, Set<String>> scoreAndPlayers : orderByScore(SaveMaxScore.readScores()).entrySet()) {
-            for (String email : scoreAndPlayers.getValue()) {
-                System.out.format("%45s %d%n", email, scoreAndPlayers.getKey());
-            }
-        }
-    }*/
-
-    /*private static Map<Integer, Set<String>> orderByScore(Map<String, Integer> scoresByPlayer) {
-        Map<Integer, Set<String>> playersByScore = new TreeMap<>();
-        for (Map.Entry<String, Integer> playerAndScore : scoresByPlayer.entrySet()) {
-            if (!playersByScore.containsKey(playerAndScore.getValue())) {
-                playersByScore.put(playerAndScore.getValue(), new HashSet<String>());
-            }
-            playersByScore.get(playerAndScore.getValue()).add(playerAndScore.getKey());
-        }
-        return playersByScore;
-    }*/
-
-	public StorageService getStorageSvc() {
-		return storageSvc;
-	}
 }
