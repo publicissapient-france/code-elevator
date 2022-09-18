72611d99f34ac48e0be845062ed38d80da49f409
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2022-09-18 19:19:21 +0200

    remove scala warnings

--- elevator-core/src/main/scala/elevator/engine/scala/OmnibusElevatorManager.scala
+++ elevator-core/src/main/scala/elevator/engine/scala/OmnibusElevatorManager.scala
@@ -1,57 +1,58 @@
 package elevator.engine.scala
 
 import elevator.Command._
-import elevator.{Command, Direction}
 import elevator.Direction.{DOWN => dDOWN, UP => dUP}
 import elevator.engine.ElevatorEngine
 import elevator.engine.ElevatorEngine._
 import elevator.user.User
+import elevator.{Command, Direction}
 
 case class OmnibusElevator(
   floor: Integer = 0,
   direction: Direction = dUP,
   nextCommand: Command = UP) {
-  def withFloor(f: Int) = copy(floor = f)
-  def withDirection(d: Direction) = copy(direction = d)
-  def withNextState(c: Command) = copy(nextCommand = c)
-  def next = OmnibusElevator.this match {
-    case OmnibusElevator(_           , _      , OPEN ) => withNextState(CLOSE)
-    case OmnibusElevator(HIGHER_FLOOR, _      , CLOSE) => withDirection(dDOWN).withNextState(DOWN)
-    case OmnibusElevator(LOWER_FLOOR , _      , CLOSE) => withDirection(dUP).withNextState(UP)
-    case OmnibusElevator(_           , `dUP`  , CLOSE) => withNextState(UP)
-    case OmnibusElevator(_           , `dDOWN`, CLOSE) => withNextState(DOWN)
-    case OmnibusElevator(_           , _      , UP   ) => withFloor(floor + 1) withNextState OPEN
-    case OmnibusElevator(_           , _      , DOWN ) => withFloor(floor - 1) withNextState OPEN
+  def withFloor(f: Int): OmnibusElevator = copy(floor = f)
+  def withDirection(d: Direction): OmnibusElevator = copy(direction = d)
+  def withNextState(c: Command): OmnibusElevator = copy(nextCommand = c)
+  def next: OmnibusElevator = OmnibusElevator.this match {
+    case OmnibusElevator(_           , _      , OPEN   ) => withNextState(CLOSE)
+    case OmnibusElevator(HIGHER_FLOOR, _      , CLOSE  ) => withDirection(dDOWN).withNextState(DOWN)
+    case OmnibusElevator(LOWER_FLOOR , _      , CLOSE  ) => withDirection(dUP).withNextState(UP)
+    case OmnibusElevator(_           , `dUP`  , CLOSE  ) => withNextState(UP)
+    case OmnibusElevator(_           , `dDOWN`, CLOSE  ) => withNextState(DOWN)
+    case OmnibusElevator(_           , _      , UP     ) => withFloor(floor + 1).withNextState(OPEN)
+    case OmnibusElevator(_           , _      , DOWN   ) => withFloor(floor - 1).withNextState(OPEN)
+    case OmnibusElevator(_           , _      , NOTHING) => this
   }
 }
 
 class OmnibusElevatorManager extends ElevatorEngine {
 
-  var elevator = OmnibusElevator(0, dUP, OPEN)
+  private var elevator = OmnibusElevator(0, dUP, OPEN)
 
-  def nextCommand = {
+  def nextCommand: Command = {
     val currentState = elevator.nextCommand
     elevator = elevator.next
     currentState
   }
 
-  def call(atFloor: Integer, to: Direction) = {
+  def call(atFloor: Integer, to: Direction): OmnibusElevatorManager = {
     if (atFloor > elevator.floor) elevator = elevator withDirection dUP
     if (atFloor < elevator.floor) elevator = elevator withDirection dDOWN
     OmnibusElevatorManager.this
   }
 
-  def go(floorToGo: Integer) = {
+  def go(floorToGo: Integer): OmnibusElevatorManager = {
     if (floorToGo > elevator.floor) elevator = elevator withDirection dUP
     if (floorToGo < elevator.floor) elevator = elevator withDirection dDOWN
     OmnibusElevatorManager.this
   }
 
-  def userHasEntered(user: User) = OmnibusElevatorManager.this
+  def userHasEntered(user: User): OmnibusElevatorManager = OmnibusElevatorManager.this
 
-  def userHasExited(user: User) = OmnibusElevatorManager.this
+  def userHasExited(user: User): OmnibusElevatorManager = OmnibusElevatorManager.this
 
-  def reset(cause: String) = {
+  def reset(cause: String): OmnibusElevatorManager = {
     elevator = OmnibusElevator(0, dUP, OPEN)
     OmnibusElevatorManager.this
   }
--- elevator-core/src/main/scala/elevator/engine/scala/SmartElevatorManager.scala
+++ elevator-core/src/main/scala/elevator/engine/scala/SmartElevatorManager.scala
@@ -1,56 +1,56 @@
 package elevator.engine.scala
 
-import elevator.{Command, Direction}
-import elevator.Direction.{UP => dUP, DOWN => dDOWN}
 import elevator.Command._
+import elevator.Direction.{DOWN => dDOWN, UP => dUP}
 import elevator.engine.ElevatorEngine
 import elevator.user.User
+import elevator.{Command, Direction}
 
 case class SmartElevator(
   floor: Integer = 0,
   direction: Direction = dUP,
   nextCommand: Command = UP,
   targets: List[Integer] = List[Integer]()) {
-  def withFloor(f: Integer) = copy(floor = f)
-  def withDirection(d: Direction) = copy(direction = d)
-  def withNextState(c: Command) = copy(nextCommand = c)
-  def next = SmartElevator.this match {
+  def withFloor(f: Integer): SmartElevator = copy(floor = f)
+  def withDirection(d: Direction): SmartElevator = copy(direction = d)
+  def withNextState(c: Command): SmartElevator = copy(nextCommand = c)
+  def next: SmartElevator = SmartElevator.this match {
     case SmartElevator(_, _      , OPEN, _)                              => withNextState(CLOSE)
     case SmartElevator(_, _      , _   , _) if targets.contains(floor)   => withNextState(OPEN).copy(targets = targets.filter(_ != floor))
-    case SmartElevator(_, `dUP`  , _   , _) if targets.exists(_ > floor) => withNextState(UP)   withFloor (floor + 1)
-    case SmartElevator(_, `dUP`  , _   , _) if targets.exists(_ < floor) => withNextState(DOWN) withFloor (floor - 1) withDirection dDOWN
-    case SmartElevator(_, `dDOWN`, _   , _) if targets.exists(_ < floor) => withNextState(DOWN) withFloor (floor - 1)
-    case SmartElevator(_, `dDOWN`, _   , _) if targets.exists(_ > floor) => withNextState(UP)   withFloor (floor + 1) withDirection dUP
+    case SmartElevator(_, `dUP`  , _   , _) if targets.exists(_ > floor) => withNextState(UP)  .withFloor(floor + 1)
+    case SmartElevator(_, `dUP`  , _   , _) if targets.exists(_ < floor) => withNextState(DOWN).withFloor(floor - 1).withDirection(dDOWN)
+    case SmartElevator(_, `dDOWN`, _   , _) if targets.exists(_ < floor) => withNextState(DOWN).withFloor(floor - 1)
+    case SmartElevator(_, `dDOWN`, _   , _) if targets.exists(_ > floor) => withNextState(UP)  .withFloor(floor + 1).withDirection(dUP)
     case _                                                               => withNextState(NOTHING)
   }
-  def withNewTarget(t: Integer) = copy(targets = (targets :+ t).distinct)
+  def withNewTarget(t: Integer): SmartElevator = copy(targets = (targets :+ t).distinct)
 }
 
 class SmartElevatorManager extends ElevatorEngine {
 
-  var elevator = SmartElevator(0, dUP, OPEN)
+  private var elevator = SmartElevator(0, dUP, OPEN)
 
-  def nextCommand = {
+  def nextCommand: Command = {
     val currentState = elevator.nextCommand
     elevator = elevator.next
     currentState
   }
 
-  def call(atFloor: Integer, to: Direction) = {
-    elevator = elevator withNewTarget atFloor
+  def call(atFloor: Integer, to: Direction): SmartElevatorManager = {
+    elevator = elevator.withNewTarget(atFloor)
     this
   }
 
-  def go(floorToGo: Integer) = {
-    elevator = elevator withNewTarget floorToGo
+  def go(floorToGo: Integer): SmartElevatorManager = {
+    elevator = elevator.withNewTarget(floorToGo)
     this
   }
 
-  def userHasEntered(user: User) = this
+  def userHasEntered(user: User): SmartElevatorManager = this
 
-  def userHasExited(user: User) = SmartElevatorManager.this
+  def userHasExited(user: User): SmartElevatorManager = this
 
-  def reset(cause: String) = {
+  def reset(cause: String): SmartElevatorManager = {
     elevator = SmartElevator(0, Direction.UP, OPEN)
     SmartElevatorManager.this
   }
